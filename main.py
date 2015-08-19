8a7c7967e1c0cfd599ef140616917a6978a4db79
Jack Norman <jwnorman91@gmail.com>
2015-08-18 23:33:58 -0700

    using different numbers ofsuits and cards

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from deck import *
 from idiots_delight import *
+import csv
 
 def main():
-	num_games = 1
+	num_games = 4000000
 	results = [0]*num_games
 	for i in range(num_games):
 		deck = Deck()
@@ -11,5 +12,25 @@ def main():
 		cards_remaining = game.play_game()
 		results[i] = cards_remaining
 	print results
+
+	# num_games = 10000
+	# results = [0]*num_games
+	# for i in range(num_games):
+	# 	deck = Deck(card_values = [1,2,3,4,5,6,7])
+	# 	deck.shuffle_deck()
+	# 	game = Idiots_Delight(deck=deck)
+	# 	cards_remaining = game.play_game()
+	# 	results[i] = cards_remaining
+	# print results
+
+	# num_games = 10000
+	# results = [0]*num_games
+	# for i in range(num_games):
+	# 	deck = Deck(suit_labels = [1,2,3,4,5,6,7,8,9,10,11,12,13], card_values = [1,2,3,4])
+	# 	deck.shuffle_deck()
+	# 	game = Idiots_Delight(deck=deck)
+	# 	cards_remaining = game.play_game()
+	# 	results[i] = cards_remaining
+	# print results
 
 main()
